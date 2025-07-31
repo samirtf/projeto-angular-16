09baae1a59760c1c4afa5f246c91d842bc7653a9
Samir Trajano Feitosa <samircc20092@gmail.com>
2025-07-30 23:10:28 -0300

    add header and top-menu components

--- projeto-angular-16/src/app/app.module.ts
+++ projeto-angular-16/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TopHeaderComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule
