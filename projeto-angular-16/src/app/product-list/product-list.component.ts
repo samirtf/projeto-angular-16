83e0cf6e9ea0694c26e3525f01b31579710b6a43
Samir Trajano Feitosa <samircc20092@gmail.com>
2025-08-05 00:01:21 -0300

    ngFor

--- projeto-angular-16/src/app/product-list/product-list.component.ts
+++ projeto-angular-16/src/app/product-list/product-list.component.ts
@@ -6,36 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  addToCart: number = 0;
-  name = "John Doe"
-  product = {
-    name: "iPhone 13",
-    price: 789,
-    color: 'Red',
-    discount: 8.5,
-    inStock: 5,
-    pImage: '/assets/images/iphone.png'
-  }
-
-  getDiscountedPrice() {
-    return (this.product.price - (this.product.price * this.product.discount / 100))
-  }
-
-  onNameChange(event: any) {
-    console.log(event.target.value);
-    this.name = event.target.value;
-  }
-
-  decrementCartValue() {
-    if (this.addToCart > 0) {
-      this.addToCart--;
-    }
-  }
-
-  incrementCartValue() {
-    if (this.addToCart < this.product.inStock) {
-      this.addToCart++;
-    }
-  }
+  listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah'];
 
 }
